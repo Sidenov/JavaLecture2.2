7a9e4c8379aecdb20a6505aca0320d994c8b7783
Александр Сиденов <sid_san@mail.ru>
2022-02-05 20:14:00 +0800

    исправление

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int wallet = 100;            // Текущий баланс кошелька
-        int refill = 1200;           // Сумма пополнения
+        int refill = 1100;           // Сумма пополнения
         int bonus = refill / 100;    // Бонус за пополнение
         // int walletsumm;           // Баланс кошелька после пополнения
         // if (refill > 1000) {
@@ -9,7 +9,8 @@ public class Main {
         // } else {
         //   walletsumm = wallet + refill;
         //   }
-        int walletsumm = refill > 1000 ? wallet + refill + bonus : wallet + refill;
-        System.out.println("Баланс кошелька = " + walletsumm);
+        int walletSumm = refill > 1000 ? wallet + refill + bonus : wallet + refill;
+        System.out.println("Итоговый счет = " + walletSumm);
+        System.out.println("Количество бонусных рублей = " + bonus);
     }
 }
